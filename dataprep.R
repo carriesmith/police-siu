8a13de847cb6ca18e11fca440f00078abe68f62b
carriesmith <carrie.elizabeth@gmail.com>
2015-05-01 23:00:05 -0400

    Explore Windsor London Hamilton normalize to per 100K population

--- dataprep.R
+++ dataprep.R
@@ -1,6 +1,6 @@
 ## Libraries ####
 library(magrittr)
-
+library(dplyr)
 
 ## Read Data ####
 
@@ -60,7 +60,11 @@ xtabs(~cut(Total.Cases, breaks=c(-1,0,1,2,5,10,20,50,100)) + Year, data=dat)
 plot(Total.Cases ~ Year, data=dat)
 dat[which.max(dat$Total.Cases),]
 
+# Population of Toronto 2.5 million
+# Normalize to per-million
 dat.to <- dat[dat$Police.Service=="Toronto Police Service",]
+dat.to[,3:11] <- dat.to[,3:11]/25
+
 plot(Total.Cases ~ Year, data=dat.to)
 plot(Firearm.Injuries ~ Year, data=dat.to)
 plot(Firearm.Deaths ~ Year, data=dat.to)
@@ -84,7 +88,10 @@ summary(fit)
 grep("Hamilton", dat$Police.Service)
 dat$Police.Service[grep("Hamilton", dat$Police.Service)]
 
+# Population of Hamilton .5 million
+# Normalize to per-million
 dat.ha <- dat[dat$Police.Service=="Hamilton Police Service",]
+dat.ha[,3:11] <- dat.ha[,3:11]/5
 
 plot(Total.Cases ~ Year, data=dat.ha)
 plot(Firearm.Injuries ~ Year, data=dat.ha)
@@ -96,4 +103,32 @@ plot(Vehicle.Deaths ~ Year, data=dat.ha)
 plot(Sexual.Assault.Complaints ~ Year, data=dat.ha)
 
 fit <- lm(Total.Cases ~ Year, data=dat.ha)
+summary(fit)
+
+
+
+## London ####
+
+unique(dat$Police.Service[grep("London", dat$Police.Service)])
+
+# Population of London 350 thousand
+# Normalize to per-million
+dat.lo <- dat[dat$Police.Service=="London Police Service",]
+dat.lo[,3:11] <- dat.lo[,3:11]/3.5
+plot(Total.Cases ~ Year, data=dat.lo)
+fit <- lm(Total.Cases ~ Year, data=dat.lo)
+summary(fit)
+
+
+
+## Windsor ####
+
+unique(dat$Police.Service[grep("Windsor", dat$Police.Service)])
+
+# Population of Windsor 220 thousand
+# Normalize to per-million
+dat.win <- dat[dat$Police.Service=="Windsor Police Service",]
+dat.win[,3:11] <- dat.win[,3:11]/2.2
+plot(Total.Cases ~ Year, data=dat.win)
+fit <- lm(Total.Cases ~ Year, data=dat.win)
 summary(fit)
